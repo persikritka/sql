328bbd00f902367f5f1128d50d63c06d8494fdde
persikritka <121298973+persikritka@users.noreply.github.com>
2023-09-23 20:20:33 +0300

    all is good

--- src/main/java/service/impl/PersonServiceImpl.java
+++ src/main/java/service/impl/PersonServiceImpl.java
@@ -1,12 +1,22 @@
 package service.impl;
 
+import database.ConnectorToDataBase;
 import service.PersonService;
 
 import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class PersonServiceImpl implements PersonService {
+    private ConnectorToDataBase connectorToDataBase;
+    public PersonServiceImpl(){
+        connectorToDataBase=new ConnectorToDataBase();
+    }
     @Override
     public ResultSet getAllDataPerson() {
-        return null;
+        try {
+            return connectorToDataBase.getStatement().executeQuery("select * from employer");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
