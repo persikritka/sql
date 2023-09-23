0e506d14b85ccc6524ab7c33e67cf566369f078c
persikritka <121298973+persikritka@users.noreply.github.com>
2023-09-23 20:16:24 +0300

    all is good

---
+++ src/main/java/service/impl/PersonServiceImpl.java
@@ -0,0 +1,12 @@
+package service.impl;
+
+import service.PersonService;
+
+import java.sql.ResultSet;
+
+public class PersonServiceImpl implements PersonService {
+    @Override
+    public ResultSet getAllDataPerson() {
+        return null;
+    }
+}
